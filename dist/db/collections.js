fe941ddf4b3b864a14e8753486937bb2d3baef9f
Taariq Isaacs <taariq@uber5.com>
2017-05-17 18:32:45 +0200

    added dist folder to gitignore

--- dist/db/collections.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-const ROOMS = exports.ROOMS = 'rooms';
-const ROOMMATES = exports.ROOMMATES = 'roommates';
-//# sourceMappingURL=collections.js.map
